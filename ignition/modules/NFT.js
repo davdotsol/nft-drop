9119cdca0d1de5b5fdfc16823c1d924da7b1990b
dvauquelin <david.vauquelin@planisware.com>
2024-05-14 17:31:26 +0200

    Added deployment scripts

---
+++ ignition/modules/NFT.js
@@ -0,0 +1,29 @@
+const { buildModule } = require('@nomicfoundation/hardhat-ignition/modules');
+const { ethers } = require('hardhat');
+
+const NAME = '';
+const SYMBOL = '';
+const COST = ethers.parseEther('10');
+const MAX_SUPPLY = 25;
+const NFT_MINT_DATE = (Date.now() + 60000).toString().slice(0, 10);
+const IPFS_METADATA_URI =
+  'ipfs://QmQPEMsfd1tJnqYPbnTQCjoa8vczfsV1FmqZWgRdNQ7z3g/';
+
+module.exports = buildModule('NFTModule', (m) => {
+  const name = m.getParameter('name', NAME);
+  const symbol = m.getParameter('symbol', SYMBOL);
+  const cost = m.getParameter('cost', COST);
+  const max_supply = m.getParameter('max_supply', MAX_SUPPLY);
+  const mint_date = m.getParameter('mint_date', NFT_MINT_DATE);
+  const ipfs_uri = m.getParameter('ipfs_uri', IPFS_METADATA_URI);
+
+  const nft = m.contract('NFT', [
+    name,
+    symbol,
+    cost,
+    max_supply,
+    mint_date,
+    ipfs_uri,
+  ]);
+  return { nft };
+});
